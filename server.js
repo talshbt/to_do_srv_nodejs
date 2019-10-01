8ec09749d00ce41b390ea90166023aebfeb74a6e
Tal Shabtai <talshbt@gmail.com>
2019-10-01 18:17:11 +0300

    update

---
+++ server.js
@@ -0,0 +1,162 @@
+  const bodyParser = require('body-parser');
+  const express = require('express')
+  const PORT = process.env.PORT || 5555;
+  const app = express();
+
+  app.use(bodyParser.urlencoded({extended: false}));
+  app.use(bodyParser.json());
+
+
+
+/*****************************************************************************/
+var dbController = (function() {
+          const sqlite3 = require('sqlite3').verbose();
+        
+
+    return {
+
+       
+
+        initDB: function() {
+            let db = new sqlite3.Database("./mydb.sqlite3", (err) => { 
+                if (err) { 
+                    console.log('Error when creating the database', err) 
+                } else { 
+                    console.log('Database created!') 
+                 
+                  
+                } 
+            })
+            return db;
+        },
+        read: function (){
+
+                console.log("Read data from TODONOTES");
+                db.all("SELECT rowid AS id, NoteMessage FROM TODONOTES", function(err, rows) {
+                    rows.forEach(function (row) {
+                        console.log(row.id + ": " + row.NoteMessage);
+                    });
+
+
+                });
+
+                // db.close();
+
+              },
+
+
+          initNoteTable: function (NoteMessage){
+                let sqlCommand = "CREATE TABLE IF NOT EXISTS TODONOTES(id INTEGER PRIMARY KEY AUTOINCREMENT, NoteMessage TEXT)";
+                db.run(sqlCommand);
+                console.log("create database table TODONOTES");
+          },    
+        
+          addNewNote: function (NoteMessage){
+                db.run('INSERT INTO TODONOTES (NoteMessage) VALUES (?)', [NoteMessage]);
+                console.log("Inserted new Note:",NoteMessage);
+          },
+          
+          //this method will remove all data, and reset all id's
+          truncate: function(){
+            //db.run("DELETE FROM TODONOTES;UPDATE SQLITE_SEQUENCE SET SEQ=0 WHERE NAME='TODONOTES';");
+            db.run("DROP TABLE TODONOTES;");
+            this.initNoteTable();
+            console.log("TODONOTES table truncated...");
+
+            },
+
+          deleteNote: function (id){
+                db.run(
+                  `DELETE FROM TODONOTES WHERE id = ?`,
+                  [id],
+                
+                )
+            },
+
+            //this method will remove all data, without resetting id's
+            deleteAll: function (){
+
+                db.run(`DELETE FROM TODONOTES;`)
+            },
+         
+            update: function(NoteMessage, id){
+                const stmt = db.prepare('UPDATE TODONOTES SET NoteMessage = ? WHERE id = ?'); 
+                const updates = stmt.run(NoteMessage, id);
+            }
+           
+          
+    };
+    
+})();
+
+/*****************************************************************************/
+//pre-step init DB and create(if it is not exists from last time... create noteMSG table)
+
+var db = dbController.initDB();
+dbController.initNoteTable();
+dbController.truncate();
+
+app.get('/', function(req,res){
+  res.send("Welcome to Invoicing App");
+});
+
+app.listen(PORT, function(){
+  console.log(`App running on localhost:${PORT}`);
+});
+
+app.get("/createTable", function(req, res) {
+
+  db = dbController.createTable();  
+
+  res.send("createTable Succses");
+
+});
+
+app.post("/addNote", function(req, res) {
+  
+  let noteMSG = req.body.NoteMessage;
+  dbController.addNewNote(noteMSG);
+
+  console.log(noteMSG);
+  
+  
+//   res.json({
+//     "status": true,
+//     "message": "insert "
+// });
+  dbController.read();
+  res.send(`${noteMSG} added.`);
+
+});
+
+
+app.post("/deleteNote", function(req, res) {
+  
+  let test = req.body.id;
+  dbController.deleteNote(test);
+  console.log("---------------remove now " + test);
+  dbController.read();
+
+
+});
+
+app.get("/deleteAll", function(req, res) {
+  
+  dbController.deleteAll();
+  console.log("---------------remove all now ");
+  res.send("remove all success");
+  
+  db = dbController.createTable();  
+  dbController.read();
+
+});
+
+app.get("/truncate", function(req, res) {
+  
+    dbController.truncate();
+    console.log("---------------truncate all now ");
+    res.send("truncate all success");
+    
+    dbController.read();
+  
+  });
